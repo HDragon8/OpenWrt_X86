ced98b0fc49e60d7cf0f5864c348b31093e78055
noodles <houyonghua2015@gmail.com>
2020-04-08 08:26:10 +0800

    ###

--- diy.sh
+++ diy.sh
@@ -35,18 +35,18 @@ sed -i 's/OpenWrt/@noodles build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/d
 
 # 替换index
 # echo ''
-rm -rf feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-cp noodlesindex/index.htm feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+#rm -rf feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+#cp noodlesindex/index.htm feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 # 替换x86_index
-rm -rf package/lean/autocore/files/index.htm
-cp noodlesindex/x86_index.htm package/lean/autocore/files/index.htm
+#rm -rf package/lean/autocore/files/index.htm
+#cp noodlesindex/x86_index.htm package/lean/autocore/files/index.htm
 
 # 添加天气预报翻译 
-sed -i '$a \       ' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '$a #天气预报' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '$a msgid "Weather"' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '$a msgstr "天气"' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '$a \       ' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '$a msgid "Local Weather"' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '$a msgstr "本地天气"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#sed -i '$a \       ' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#sed -i '$a #天气预报' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#sed -i '$a msgid "Weather"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#sed -i '$a msgstr "天气"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#sed -i '$a \       ' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#sed -i '$a msgid "Local Weather"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#sed -i '$a msgstr "本地天气"' feeds/luci/modules/luci-base/po/zh-cn/base.po
